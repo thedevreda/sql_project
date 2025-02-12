690b51c89b3afff8055aac9d85058c50a306fbfe
thedevreda <redaamelloul@gmail.com>
2025-02-12 23:32:38 +0100

    Finishing the project

--- project_sql/2-skills_top_jobs.sql
+++ project_sql/2-skills_top_jobs.sql
@@ -43,8 +43,14 @@ There are the top10 dat anayst roles i found in job postings in 2023, can you an
 
 /* On data results beside the save data you have open reslats as json click on it / copy paste here*/
 
-
+  
 /*
+Here's the breakdown of the most demanded skills for data analysts in 2023, based on job postings:
+SQL is leading with a bold count of 8.
+Python follows closely with a bold count of 7.
+Tableau is also highly sought after, with a bold count of 6.
+Other skills like R, Snowflake, Pandas, and Excel show varying degrees of demand.
+
 [
   {
     "job_id": 552322,
--- project_sql/3-top_demanded_skills.sql
+++ project_sql/3-top_demanded_skills.sql
@@ -1,5 +1,10 @@
 /*
-    What skills are most in demand for data analysts?
+Question: What are the most in-demand skills for data analysts?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data analyst.
+- Focus on all job postings.
+- Why? Retrieves the top 5 skills with the highest demand in the job market, 
+    providing insights into the most valuable skills for job seekers.
 */
 
 
@@ -15,5 +20,33 @@ WHERE
     job_title_short LIKE 'Data Analyst'  -- specify onyl DA
 GROUP BY skills -- grouping by skills to show only unique skills
 ORDER BY skill_count DESC
-LIMIT 5
+LIMIT 5;
 
+/*
+Here's the breakdown of the most demanded skills for data analysts in 2023
+SQL and Excel remain fundamental, emphasizing the need for strong foundational skills in data processing and spreadsheet manipulation.
+Programming and Visualization Tools like Python, Tableau, and Power BI are essential, pointing towards the increasing importance of technical skills in data storytelling and decision support.
+
+[
+  {
+    "skills": "sql",
+    "demand_count": "7291"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "4611"
+  },
+  {
+    "skills": "python",
+    "demand_count": "4330"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "3745"
+  },
+  {
+    "skills": "power bi",
+    "demand_count": "2609"
+  }
+]
+*/
